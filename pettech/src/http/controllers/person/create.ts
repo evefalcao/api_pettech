3b35a2690159889ebf0429f6c6a87714086da725
Eveline Falcão <falcao.eveline@gmail.com>
2025-02-24 08:52:12 -0300

    end of class 01 - video 10

--- pettech/src/http/controllers/person/create.ts
+++ pettech/src/http/controllers/person/create.ts
@@ -9,16 +9,18 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     name: z.string(),
     birth: z.coerce.date(),
     email: z.string().email(),
+    user_id: z.coerce.number()
   })
 
-  const { cpf, name, birth, email } = registerBodySchema.parse(request.body)
+  const { cpf, name, birth, email, user_id } = registerBodySchema.parse(request.body)
 
   try {
     const personRepository = new PersonRepository()
     const createPersonUseCase = new CreatePersonUseCase(personRepository)
 
-    await createPersonUseCase.handler({ cpf, name, birth, email })
-    reply.status(201).send()
+    const person = await createPersonUseCase.handler({ cpf, name, birth, email, user_id })
+
+    reply.status(201).send(person)
   } catch (error) {
     console.error(error)
 
